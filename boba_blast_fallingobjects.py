4cde6e97b3a8ad8cb3f2bcbd291db635e5cf9d6c
Berwin Lan <blan@olin.edu>
2021-04-26 15:26:01 -0400

    Create Tapioca and Rock objects

--- boba_blast_fallingobjects.py
+++ boba_blast_fallingobjects.py
@@ -1,7 +1,7 @@
 """
 Space to figure out FallingObjects.
 """
-from abc import ABC, abstractmethod
+
 
 # inherits from Actor class
 # y-max = maximum y-coordinate of screen (very top)
@@ -10,27 +10,57 @@ class FallingObject(Actor, ABC):
     """
     Represents a falling object (tapioca and rocks).
     """
-    def __init__(self):
+    def __init__(self, image):
         """
         Initializes an instance of a FallingObject.
         """
-        super().__init__()
+        super().__init__(image)
         self._y_value = `screen.get_height`
 
     def __repr__(self):
         return (super().x_pos, super().y_pos)
 
     @abstractmethod
-    def update(self):
+    def update(self, other):
         """
         A method to update location or remove invalid objects.
         """
-        if self.y_pos <= 0 or `there is a collision`:
+        if self.y_pos <= 0 or pygame.sprite.spritecollide()
             self.kill()
 
 class Tapioca(FallingObject):
     """
     Represents a tapioca pearl.
     """
-    tapioca = pygame.sprite.Group()
+    # tapioca = pygame.sprite.Group() <-- this could be good for collisions
+    def __init__(self, image):
+        """
+        Initializes an instance of a Tapioca with an image.
+        """
+        image = pygame.image.load('tapioca.png').convert()
+        super().__init__(image)
+    
+    def __repr__(self):
+        """
+        Returns a representation of a Tapioca object.
+        """
+        super.__repr__()
+
+class Rock(FallingObject):
+    """
+    Represents a rock.
+    """
+    def __init__(self, image):
+        """
+        Initializes an instance of a Rock with an image.
+        """
+        image = pygame.image.load('rock.png').convert()
+        super().__init__(image)
+    
+    def __repr__(self):
+        """
+        Returns a representation of a Rock object.
+        """
+        super.__repr__()
+
 
