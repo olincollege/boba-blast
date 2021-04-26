6236d189b9007cbd721f06c56e1742b8750e6899
Berwin Lan <blan@olin.edu>
2021-04-26 14:43:16 -0400

    Update model file

---
+++ boba_blast_model.py
@@ -0,0 +1,9 @@
+"""
+Boba blast game model implementation.
+"""
+import pygame
+pygame.init()
+
+# class Actor(pygame.sprite.Sprite):
+#     def __init__(self):
+#         super(Player, self).__init__()
