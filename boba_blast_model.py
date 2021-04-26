747b8a7ed5b94b74f70fe19e0ed8b8870424f518
Berwin Lan <blan@olin.edu>
2021-04-26 14:53:13 -0400

    Add __init__, __repr__, and coordinates to Actor

--- boba_blast_model.py
+++ boba_blast_model.py
@@ -4,6 +4,38 @@ Boba blast game model implementation.
 import pygame
 pygame.init()
 
-# class Actor(pygame.sprite.Sprite):
-#     def __init__(self):
-#         super(Player, self).__init__()
+
+
+class Actor():
+    """
+    Represents an actor.
+
+    Attributes:
+        _x_value: An integer representing the x-coordinate of the location.
+        _y_value: An integer representing the y-coordinate of the location.
+    """
+    def __init__(self):
+        """
+        Initializes an instance of an Actor.
+        """
+        self._x_value = None
+        self._y_value = None
+
+    def __repr__(self):
+        """
+        """
+        pass
+
+    @property
+    def x_value(self):
+        """
+        Returns the value of the _x_value attribute.
+        """
+        return self._x_value
+
+    @property
+    def y_value(self):
+        """
+        Returns the value of the _y_value attribute.
+        """
+        return self._y_value
