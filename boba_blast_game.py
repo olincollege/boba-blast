e99203530ba11e934884499f6a7147d3144c2a3a
Berwin Lan <blan@olin.edu>
2021-04-26 22:28:33 -0400

    Create images folder, complete sprites tutorial

--- boba_blast_game.py
+++ boba_blast_game.py
@@ -1,7 +1,7 @@
 """
 Creates and runs an instance of the Boba Blast game!
 """
-import pygame, random
+import pygame, random, os
 from boba_blast_view import Display
 
 pygame.init()
@@ -10,12 +10,17 @@ pygame.init()
 FPS = 60
 fpsClock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((800, 600))
+# Create display screen
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Boba Blast!")
 
-BLACK = (0, 0, 0)   # temporary background color
+BLACK = (0, 0, 0)   # PNG background color
 GREEN = (0, 255, 0)
-tapioca_image = pygame.image.load('tapioca.png')
-rock_image = pygame.image.load('rock.png')
+game_folder = os.path.dirname(__file__)     # figures out path to the folder with this file
+images_folder = os.path.join(game_folder, 'images')
+tapioca_image = pygame.image.load(os.path.join(images_folder, 'tapioca.png')).convert()
 
 all_sprites = pygame.sprite.Group()
 tapioca_sprites = pygame.sprite.Group()
@@ -25,10 +30,14 @@ class FallingObject(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(all_sprites, tapioca_sprites)
         self.image = tapioca_image
-        # self.image = pygame.Surface((50, 50))
-        # self.image.fill(GREEN)
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.center = (self.image.get_width() / 2, self.image.get_height() / 2)
+    
+    def update(self):
+        self.rect.y += 5
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.kill()
 
 falling_tapioca = FallingObject()
 
@@ -44,7 +53,7 @@ while not game_over:
     all_sprites.update()
 
     # fill background
-    screen.fill(BLACK)
+    screen.fill(GREEN)
     all_sprites.draw(screen)
 
     pygame.display.flip()
