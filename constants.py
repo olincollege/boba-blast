725f856dd18b231463cd41c4515857076e0d6dcc
Berwin Lan <blan@olin.edu>
2021-05-01 18:26:34 -0400

    Updated lives image, removed unused image files
    it really does not like the new player file, not sure why but could be a resizing issue

--- constants.py
+++ constants.py
@@ -32,9 +32,6 @@ PLAYER_IMAGE = pygame.image.load(os.path.join(IMAGES_FOLDER, 'Player(1).png'))
 PLAYER_IMAGE.set_colorkey((255, 255, 255))
 PLAYER_WIDTH, PLAYER_HEIGHT = PLAYER_IMAGE.get_size()
 
-LIVES_IMAGE = pygame.image.load(os.path.join(IMAGES_FOLDER, 'lives.png'))
-LIVES_IMAGE = pygame.transform.scale(LIVES_IMAGE, (35, 35))
-LIVES_IMAGE.set_colorkey((247, 247, 247))
-
-BOBA_IMAGE = pygame.image.load(os.path.join(IMAGES_FOLDER, 'boba.png'))
-BOBA_IMAGE.set_colorkey((247, 247, 247))
+LIVES_IMAGE = pygame.image.load(os.path.join(IMAGES_FOLDER, 'Boba-Blast(2)-lives.png'))
+LIVES_IMAGE = pygame.transform.scale(LIVES_IMAGE, (45, 45))
+LIVES_IMAGE.set_colorkey((255, 255, 255))
