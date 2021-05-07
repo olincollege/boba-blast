9f7fb65da5ee137f55d846678f5c8bc41c4af25f
Claire Hashizume <clairehashizume@gmail.com>
2021-05-07 18:18:42 -0400

    Add docstrings for controller file

--- boba_blast_controller.py
+++ boba_blast_controller.py
@@ -7,18 +7,22 @@ import pygame
 
 class GraphicalController:
     """
-    Creates an instance of a GraphicalController, which takes player input.
+    Creates an instance of a GraphicalController which takes player input.
     """
 
     def __init__(self):
         """
-        pylint
+        Initializes the object's state
         """
         pass
 
     def get_move(self):
         """
-        pylint
+        Gets the keys that the user is pressing
+
+        Returns:
+        pressed_keys: A sequence of boolean values containing the state
+        of every key on the keyboard, with True indicating a key is pressed.
         """
         pygame.init()
         pressed_keys = pygame.key.get_pressed()
@@ -26,7 +30,8 @@ class GraphicalController:
 
     def check_exit(self):
         """
-        pylint
+        Checks whether the user is wanting to exit the window and stops the
+        pygame program from running.
         """
         for event in pygame.event.get():
             # check for user closing window
